301a948a6866430367cab45706d80c24b75cd049
Rishav Bharti <rishavbharti21@gmail.com>
2023-04-26 23:32:41 +0530

    feat: add user schema

---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+import Database from '../db/index.js';
+
+const userSchema = {
+  username: {
+    type: 'String',
+    trim: true,
+    minLength: 3,
+    maxLength: 100,
+    required: true,
+  },
+  email: {
+    type: 'String',
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please enter a valid email',
+    ],
+    lowercase: true,
+    trim: true,
+    minLength: 6,
+    maxLength: 100,
+    required: true,
+  },
+  password: {
+    type: 'String',
+    trim: true,
+    minLength: 6,
+    maxLength: 70,
+    required: true,
+  },
+  preferences: {
+    type: 'Array',
+    default: [],
+  },
+  read: {
+    type: 'Array',
+    default: [],
+  },
+  favorites: {
+    type: 'Array',
+    default: [],
+  },
+};
+
+export default new Database().model('User', userSchema);
